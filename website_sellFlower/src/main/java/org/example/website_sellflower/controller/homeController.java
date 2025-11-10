f4daead7277f1588076ab0f2d5366ceea1ac8952
DDship123 <96617601+DDship123@users.noreply.github.com>
2025-11-10 10:01:26 +0700

    cập nhật

--- website_sellFlower/src/main/java/org/example/website_sellflower/controller/homeController.java
+++ website_sellFlower/src/main/java/org/example/website_sellflower/controller/homeController.java
@@ -3,6 +3,7 @@ package org.example.website_sellflower.controller;
 import jakarta.servlet.http.HttpSession;
 import org.example.website_sellflower.entity.Account;
 import org.example.website_sellflower.entity.Product;
+import org.example.website_sellflower.service.OrderService;
 import org.example.website_sellflower.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,6 +16,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class homeController {
     @Autowired
     private ProductService productService;
+
+    @Autowired
+    private OrderService orderService;
 
     @GetMapping
     public String showHome(HttpSession session, Model model){
